a113654168ad1fb132b95d206ad2fe42f51eb49c
Kiattisak9905 <81211115+Kiattisak9905@users.noreply.github.com>
2022-04-06 01:58:49 +0700

    create menu

---
+++ src/component/layouts/List.js
@@ -0,0 +1,9 @@
+import { MdMail, MdForwardToInbox } from 'react-icons/md'
+
+export const pages = [
+  { name: 'Employee', icon: <MdMail />, role: 'user', type: 'SERVICE' },
+  { name: 'Application', icon: <MdMail />, role: 'user', type: 'SERVICE' },
+  { name: 'Work', icon: <MdMail />, role: 'user', type: 'SERVICE' },
+  { name: 'Report', icon: <MdMail />, role: 'user', type: 'SERVICE' },
+  { name: 'User', icon: <MdMail />, role: 'user', type: 'SERVICE' },
+]
